8a63a553cfcf129d4eb8126b55bc165aee44d60a
JuanVargas8 <ju.vargash@duocuc.cl>
2026-09-06 18:09:30 -0300

    Feat: imagenes, historial.html, Fix: login, lógica de login y lógica de logs e historial

--- JS/registro.js
+++ JS/registro.js
@@ -4,11 +4,33 @@ function validacionRegistro() {
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirm_password").value;
 
-    if (usuario.length < 3 ) {
-        alert("El nombre de usuario debe tener al menos 3 caracteres.");
-        return false;
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{6,}$/;
+
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@(gmail\.com|outlook\.com|duocuc\.cl)$/;
+
+    const regexUsuario = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{3,}$/;
+        // Regex, validaciones de contraseña, correo y usuario
+        if (!regexPassword.test(password)) {
+            alert("La contraseña debe tener al menos 6 caracteres, una mayúscula, una minúscula, un número y un carácter especial.");
+            return false;
         }
 
+        if (!regexEmail.test(email)) {
+            alert("El correo debe ser Gmail, Outlook o Duoc UC.");
+            return false;
+        }
+
+        if (!regexUsuario.test(usuario)) {
+            alert("El usuario debe tener al menos 3 caracteres, contener al menos una letra y no tener caracteres especiales.");
+            return false;
+        }
+
+        // validaciones necesarias
+        if (usuario.length < 3 ) {
+            alert("El nombre de usuario debe tener al menos 3 caracteres.");
+            return false;
+        }
+        
         if (password.length < 6) {
             alert("La contraseña debe tener al menos 6 caracteres.");
             return false;
@@ -32,14 +54,6 @@ function validacionRegistro() {
 
     localStorage.setItem("usuarios", JSON.stringify(listaUsuarios))
     alert("¡Usuario registrado con éxito!");
+    registrarLog("El usuario " + usuario + " se registró con éxito.");
     return false;
 }
-
-// localStorage.setItem("usuarios", JSON.stringify([
-//     {
-//         usuario: "admin",
-//         email: "admin@pcforgery.cl",
-//         password: "admin123",
-//         rol: "admin"
-//     }
-// ]));
